ec42b16075243f827dec5fb887e9470509140764
HP Laptop <mvm29.11.21@outlook.com>
2022-05-24 09:12:08 +0500

    text

--- js/site.js
+++ js/site.js
@@ -1,3 +1,12 @@
-﻿let d = new Date();
-alert('*** WebSite ***');
-document.body.innerHTML = "<h1 style='color: azure'>Сегодня " +d + "</h1>"
+﻿function add(a, b) {
+    c = a + b;
+    return c;
+}
+
+function multiply(a, b) {
+    c = a * b;
+    return c;
+}
+
+result = add(multiply(36325, 9824), 777);
+alert(result);
